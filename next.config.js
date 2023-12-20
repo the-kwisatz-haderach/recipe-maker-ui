f3ad82b5660f057e70227937ac9f7ee25bc6a8a2
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-12-20 09:58:36 +0100

    Move auth redirect to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/dashboard(.*)',
+        missing: [
+          {
+            type: 'cookie',
+            key: 'session-cookie',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(signup)?',
+        has: [
+          {
+            type: 'cookie',
+            key: 'session-cookie',
+            value: '.+',
+          },
+        ],
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
